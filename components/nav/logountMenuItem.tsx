19d2a23615c5fd901f8827351cfc5e1fd737becc
Tomas Zifcak <tomas.zifcak197@gmail.com>
2024-01-02 20:16:55 +0100

    faster feeling logout

--- components/nav/logountMenuItem.tsx
+++ components/nav/logountMenuItem.tsx
@@ -2,11 +2,15 @@
 
 import { signOut } from "next-auth/react";
 import { DropdownMenuItem } from "../ui/dropdown-menu";
+import { useRouter } from "next/navigation";
 
 export default function LogoutMenuItem() {
+  const router = useRouter();
+
   return (
     <DropdownMenuItem
       onClick={() => {
+        router.push("/");
         void signOut({ callbackUrl: "/" });
       }}
     >
