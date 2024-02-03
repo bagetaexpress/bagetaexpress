dafa69961a92ef2759e1ea3004caabfbdcf121fb
Tomas Zifcak <tomas.zifcak197@gmail.com>
2024-02-03 23:08:03 +0100

    setTimeout before after

--- app/auth/e/dashboard/_components/printOrderLabels.tsx
+++ app/auth/e/dashboard/_components/printOrderLabels.tsx
@@ -26,10 +26,14 @@ export default function PrintOrderLabels({ orders, store }: IProps) {
 
         document.body.replaceChildren(cloned);
 
+        await new Promise((resolve) => {
+          setTimeout(resolve, 1000);
+        });
+
         window.print();
 
         await new Promise((resolve) => {
-          setTimeout(resolve, 0);
+          setTimeout(resolve, 5000);
         });
 
         document.body.replaceChildren(...originalContents);
