1407034ab564d28154d4c706eebdc92c72cff828
Tomas Zifcak <tomas.zifcak197@gmail.com>
2024-02-03 21:37:16 +0100

    maybe fix print for mobile

--- app/auth/e/dashboard/_components/printOrderList.tsx
+++ app/auth/e/dashboard/_components/printOrderList.tsx
@@ -39,122 +39,119 @@ export default function PrintOrderList({ orders, store, school }: IProps) {
       >
         <TableProperties />
       </Button>
-      <div className="hidden">
+      <div
+        className="printable"
+        ref={toPrintRef}
+        style={{ display: "flex", flexDirection: "column", gap: "1rem" }}
+      >
+        <h1 className="font-bold text-4xl">{store.name}</h1>
+        <div>
+          <p className=" font-bold text-sm">Odoberateľ:</p>
+          <h3 className=" text-2xl">{school.name}</h3>
+        </div>
+        <table>
+          <thead>
+            <tr>
+              <th rowSpan={2} scope="col">
+                p.č.
+              </th>
+              <th rowSpan={2} scope="col">
+                Názov a druh
+              </th>
+              <th rowSpan={2} scope="col">
+                Množstvo
+              </th>
+              <th colSpan={2} scope="col">
+                DPH
+              </th>
+              <th rowSpan={2} scope="col">
+                DPH
+              </th>
+            </tr>
+            <tr>
+              <th scope="col">
+                Jednotkova
+                <br />
+                cena
+              </th>
+              <th scope="col">Spolu</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order, i) => (
+              <tr key={i}>
+                <td scope="row">{i + 1}</td>
+                <td style={{ textAlign: "left" }}>{order.item.name}</td>
+                <td>{order.quantity}ks</td>
+                <td>{order.item.price}</td>
+                <td>
+                  {(parseFloat(order.item.price) * order.quantity).toFixed(2)}
+                </td>
+                <td>20%</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
         <div
-          className="printable"
-          ref={toPrintRef}
-          style={{ display: "flex", flexDirection: "column", gap: "1rem" }}
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr",
+          }}
         >
-          <h1 className="font-bold text-4xl">{store.name}</h1>
           <div>
-            <p className=" font-bold text-sm">Odoberateľ:</p>
-            <h3 className=" text-2xl">{school.name}</h3>
+            <p>
+              Spolu: {orders.reduce((acc, { quantity }) => acc + +quantity, 0)}
+              ks
+            </p>
           </div>
-          <table>
-            <thead>
-              <tr>
-                <th rowSpan={2} scope="col">
-                  p.č.
-                </th>
-                <th rowSpan={2} scope="col">
-                  Názov a druh
-                </th>
-                <th rowSpan={2} scope="col">
-                  Množstvo
-                </th>
-                <th colSpan={2} scope="col">
-                  DPH
-                </th>
-                <th rowSpan={2} scope="col">
-                  DPH
-                </th>
-              </tr>
-              <tr>
-                <th scope="col">
-                  Jednotkova
-                  <br />
-                  cena
-                </th>
-                <th scope="col">Spolu</th>
-              </tr>
-            </thead>
-            <tbody>
-              {orders.map((order, i) => (
-                <tr key={i}>
-                  <td scope="row">{i + 1}</td>
-                  <td style={{ textAlign: "left" }}>{order.item.name}</td>
-                  <td>{order.quantity}ks</td>
-                  <td>{order.item.price}</td>
-                  <td>
-                    {(parseFloat(order.item.price) * order.quantity).toFixed(2)}
-                  </td>
-                  <td>20%</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <div>
+            <p>
+              Celková suma:
+              {orders
+                .reduce(
+                  (acc, order) =>
+                    acc + parseFloat(order.item.price) * order.quantity,
+                  0
+                )
+                .toFixed(2)}
+              €
+            </p>
+          </div>
+        </div>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr",
+          }}
+        >
           <div
             style={{
-              display: "grid",
-              gridTemplateColumns: "1fr 1fr",
+              display: "flex",
+              flexDirection: "column",
+              paddingTop: "1rem",
+              paddingBottom: "1rem",
+              gap: "1rem",
             }}
           >
-            <div>
-              <p>
-                Spolu:{" "}
-                {orders.reduce((acc, { quantity }) => acc + +quantity, 0)}
-                ks
-              </p>
-            </div>
-            <div>
-              <p>
-                Celková suma:
-                {orders
-                  .reduce(
-                    (acc, order) =>
-                      acc + parseFloat(order.item.price) * order.quantity,
-                    0
-                  )
-                  .toFixed(2)}
-                €
-              </p>
-            </div>
+            <p>Kontroloval:</p>
+            <p>Podpis skladníka:</p>
           </div>
           <div
             style={{
-              display: "grid",
-              gridTemplateColumns: "1fr 1fr",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "flex-end",
             }}
           >
-            <div
+            <p
               style={{
-                display: "flex",
-                flexDirection: "column",
-                paddingTop: "1rem",
-                paddingBottom: "1rem",
-                gap: "1rem",
+                paddingLeft: "2rem",
+                paddingRight: "2rem",
+                borderTop: "2px solid black",
               }}
             >
-              <p>Kontroloval:</p>
-              <p>Podpis skladníka:</p>
-            </div>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "flex-end",
-              }}
-            >
-              <p
-                style={{
-                  paddingLeft: "2rem",
-                  paddingRight: "2rem",
-                  borderTop: "2px solid black",
-                }}
-              >
-                Dátum, podpis a pečia
-              </p>
-            </div>
+              Dátum, podpis a pečia
+            </p>
           </div>
         </div>
       </div>
