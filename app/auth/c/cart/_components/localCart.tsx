7651ef9363e4d693b16a88542eadcfcef7f43d3e
Tomas Zifcak <tomas.zifcak197@gmail.com>
2024-02-07 23:56:20 +0100

    forgot files

---
+++ app/auth/c/cart/_components/localCart.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { Item } from "@/db/schema";
+import CartItemRow from "./cartItemRow";
+import { useState } from "react";
+
+interface LocalCartProps {
+  data: {
+    item: Item;
+    quantity: number;
+  }[];
+  cartId: string;
+}
+
+export default function LocalCart({
+  data: originalData,
+  cartId,
+}: LocalCartProps) {
+  const [data, setData] = useState(originalData);
+
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold pt-2">Košík</h1>
+      <div className="grid grid-cols-1 divide-y-2">
+        {data.map((item, i) => (
+          <CartItemRow
+            key={item.item.id + "-" + i}
+            {...item.item}
+            quantity={item.quantity}
+            addItem={() => {
+              const newData = data.map((d) => {
+                if (d.item.id === item.item.id) {
+                  return { ...d, quantity: d.quantity + 1 };
+                }
+                return d;
+              });
+              setData(newData);
+            }}
+            removeItem={() => {
+              const newData = data.map((d) => {
+                if (d.item.id === item.item.id) {
+                  return { ...d, quantity: d.quantity - 1 };
+                }
+                return d;
+              });
+              setData(newData);
+            }}
+            cartId={cartId}
+          />
+        ))}
+      </div>
+      <div className="flex justify-between py-4">
+        <p className="font-semibold text-lg">Spolu</p>
+        <p className="font-semibold text-xl">
+          {data
+            .reduce(
+              (acc, item) => acc + parseFloat(item.item.price) * item.quantity,
+              0
+            )
+            .toFixed(2)}
+          €
+        </p>
+      </div>
+    </div>
+  );
+}
