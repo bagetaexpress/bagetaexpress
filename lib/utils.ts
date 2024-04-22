b9469a248cb4c304c369d0d0dfe942172fdcec89
Tomas Zifcak <tomas.zifcak197@gmail.com>
2024-04-22 12:37:51 +0200

    test timezone 3

--- lib/utils.ts
+++ lib/utils.ts
@@ -18,8 +18,9 @@ export function getDate(date_str: string): Date {
 export function getNewDate(): Date {
   // return new Date();
   const date = new Date();
-  const offset = date.getTimezoneOffset() * 60 * 1000;
-  const time = date.getTime() - offset;
+  //const offset = date.getTimezoneOffset() * 60 * 1000;
+  const offset = 120 * 60 * 1000;
+  const time = date.getTime() + offset;
 
   return new Date(time);
 }
