cbd7190949b22f0a3452f3eaedfef7ab119b75b3
Tomas Zifcak <tomas.zifcak197@gmail.com>
2024-09-09 08:05:50 +0200

    ui: landing page refactor

--- app/_components/login-services.tsx
+++ app/_components/login-services.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import MsLogo from "@/assets/ms-symbollockup.svg";
@@ -13,7 +13,12 @@ export default function LoginServices() {
   if (session && session.user) {
     return (
       <Button
-        className="w-full"
+        style={{
+          backgroundColor: "hsl(var(--foreground))",
+          color: "hsl(var(--background))",
+        }}
+        size="lg"
+        className="px-3"
         onClick={() => {
           switch (true) {
             case session.user.isEmployee:
@@ -38,8 +43,12 @@ export default function LoginServices() {
   return (
     <>
       <Button
-        variant="outline"
-        className=" px-3 gap-3"
+        style={{
+          backgroundColor: "hsl(var(--foreground))",
+          color: "hsl(var(--background))",
+        }}
+        size="lg"
+        className="px-3 gap-3"
         onClick={() => {
           signIn("azure-ad", { callbackUrl: "/auth/redirect" });
         }}
---
+++ components/blob.tsx
@@ -0,0 +1,226 @@
+import React from "react";
+
+import { CSSProperties } from "react";
+
+export function BlobFullPrimary({
+  className,
+  style,
+}: {
+  className?: string;
+  style?: CSSProperties;
+}) {
+  return (
+    <div
+      style={{
+        ...style,
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+        backgroundRepeat: "repeat",
+        backgroundImage: `url("data:image/svg+xml;utf8,%3Csvg viewBox=%220 0 1000 1000%22 xmlns=%22http:%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cdefs%3E%3CclipPath id=%22a%22%3E%3Cpath fill=%22currentColor%22 d=%22M764.5 655.5Q748 811 576 866.5T311 779q-93-143-89.5-277.5T313 216q88-151 269-98.5T772 335q9 165-7.5 320.5Z%22%2F%3E%3C%2FclipPath%3E%3C%2Fdefs%3E%3Cg clip-path=%22url(%23a)%22%3E%3Cpath fill=%22%23ffff8d%22 d=%22M764.5 655.5Q748 811 576 866.5T311 779q-93-143-89.5-277.5T313 216q88-151 269-98.5T772 335q9 165-7.5 320.5Z%22%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E")`,
+      }}
+      className={className}
+    ></div>
+  );
+}
+
+export function BlobFullPrimary2({
+  className,
+  style,
+}: {
+  className?: string;
+  style?: CSSProperties;
+}) {
+  return (
+    <div
+      style={{
+        ...style,
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+        backgroundRepeat: "repeat",
+        backgroundImage: `url("data:image/svg+xml;utf8,%3Csvg viewBox=%220 0 1000 1000%22 xmlns=%22http:%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cdefs%3E%3CclipPath id=%22a%22%3E%3Cpath fill=%22currentColor%22 d=%22M840.5 663.5Q761 827 592 833t-331-67Q99 693 95 498t153-304q157-109 311-35t257.5 207.5q103.5 133.5 24 297Z%22%2F%3E%3C%2FclipPath%3E%3C%2Fdefs%3E%3Cg clip-path=%22url(%23a)%22%3E%3Cpath fill=%22%23ffff8d%22 d=%22M840.5 663.5Q761 827 592 833t-331-67Q99 693 95 498t153-304q157-109 311-35t257.5 207.5q103.5 133.5 24 297Z%22%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E")`,
+      }}
+      className={className}
+    ></div>
+  );
+}
+
+export function BlobPatternBlack({
+  className,
+  style,
+}: {
+  className?: string;
+  style?: CSSProperties;
+}) {
+  return (
+    <div
+      style={{
+        ...style,
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+        backgroundRepeat: "repeat",
+        backgroundImage: `url("data:image/svg+xml;utf8,%3Csvg viewBox=%220 0 1000 1000%22 xmlns=%22http:%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cdefs%3E%3CclipPath id=%22a%22%3E%3Cpath fill=%22currentColor%22 d=%22M816.5 657Q681 814 494.5 823T250 666q-58-166 13-309.5T539 145q205-68 309 143.5T816.5 657Z%22%2F%3E%3C%2FclipPath%3E%3Cpattern id=%22b%22 patternUnits=%22userSpaceOnUse%22 width=%2225%22 height=%2225%22 viewBox=%220 0 100 100%22 fill=%22%23212121%22%3E%3Ccircle cx=%2250%22 cy=%2250%22 r=%2212.5%22%2F%3E%3C%2Fpattern%3E%3C%2Fdefs%3E%3Cg clip-path=%22url(%23a)%22%3E%3Cpath fill=%22url(%23b)%22 d=%22M816.5 657Q681 814 494.5 823T250 666q-58-166 13-309.5T539 145q205-68 309 143.5T816.5 657Z%22%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E")`,
+      }}
+      className={className}
+    ></div>
+  );
+}
+
+export function BlobPatternBlack2({
+  className,
+  style,
+}: {
+  className?: string;
+  style?: CSSProperties;
+}) {
+  return (
+    <div
+      style={{
+        ...style,
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+        backgroundRepeat: "repeat",
+        backgroundImage: `url("data:image/svg+xml;utf8,%3Csvg viewBox=%220 0 1000 1000%22 xmlns=%22http:%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cdefs%3E%3CclipPath id=%22a%22%3E%3Cpath fill=%22currentColor%22 d=%22M814 616.5Q686 733 551 800t-245-43q-110-110-93-248.5T316 221q86-149 230-54.5t270 214q126 119.5-2 236Z%22%2F%3E%3C%2FclipPath%3E%3Cpattern id=%22b%22 patternUnits=%22userSpaceOnUse%22 width=%2225%22 height=%2225%22 viewBox=%220 0 100 100%22 fill=%22%23212121%22%3E%3Ccircle cx=%2250%22 cy=%2250%22 r=%2212.5%22%2F%3E%3C%2Fpattern%3E%3C%2Fdefs%3E%3Cg clip-path=%22url(%23a)%22%3E%3Cpath fill=%22url(%23b)%22 d=%22M814 616.5Q686 733 551 800t-245-43q-110-110-93-248.5T316 221q86-149 230-54.5t270 214q126 119.5-2 236Z%22%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E")`,
+      }}
+      className={className}
+    ></div>
+  );
+}
+
+export function BlobOutlineSecondary({
+  className,
+  style,
+}: {
+  className?: string;
+  style?: CSSProperties;
+}) {
+  return (
+    <div
+      style={{
+        ...style,
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+        backgroundRepeat: "repeat",
+        backgroundImage: `url("data:image/svg+xml;utf8,%3Csvg viewBox=%220 0 1000 1000%22 xmlns=%22http:%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cdefs%3E%3CclipPath id=%22a%22%3E%3Cpath fill=%22currentColor%22 d=%22M765.5 624Q698 748 546 855.5t-255-49Q188 650 172.5 492.5T297 280q140-55 318-101.5T813 316q20 184-47.5 308Z%22%2F%3E%3C%2FclipPath%3E%3C%2Fdefs%3E%3Cg clip-path=%22url(%23a)%22%3E%3Cpath fill=%22none%22 stroke=%22%23ffb84d%22 stroke-width=%2220%22 d=%22M765.5 624Q698 748 546 855.5t-255-49Q188 650 172.5 492.5T297 280q140-55 318-101.5T813 316q20 184-47.5 308Z%22%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E")`,
+      }}
+      className={className}
+    ></div>
+  );
+}
+
+export function BlobOutlineSecondary2({
+  className,
+  style,
+}: {
+  className?: string;
+  style?: CSSProperties;
+}) {
+  return (
+    <div
+      style={{
+        ...style,
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+        backgroundRepeat: "repeat",
+        backgroundImage: `url("data:image/svg+xml;utf8,%3Csvg viewBox=%220 0 1000 1000%22 xmlns=%22http:%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cdefs%3E%3CclipPath id=%22a%22%3E%3Cpath fill=%22currentColor%22 d=%22M816 649.5q41 149.5-109.5 159t-292 46.5Q273 892 186 769t-40.5-252q46.5-129 133-190.5T469 175q104-90 212.5-4.5t101 207.5Q775 500 816 649.5Z%22%2F%3E%3C%2FclipPath%3E%3C%2Fdefs%3E%3Cg clip-path=%22url(%23a)%22%3E%3Cpath fill=%22none%22 stroke=%22%23ffb84d%22 stroke-width=%2220%22 d=%22M816 649.5q41 149.5-109.5 159t-292 46.5Q273 892 186 769t-40.5-252q46.5-129 133-190.5T469 175q104-90 212.5-4.5t101 207.5Q775 500 816 649.5Z%22%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E")`,
+      }}
+      className={className}
+    ></div>
+  );
+}
+
+export function BlobOutlineWhite({
+  className,
+  style,
+}: {
+  className?: string;
+  style?: CSSProperties;
+}) {
+  return (
+    <div
+      style={{
+        ...style,
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+        backgroundRepeat: "repeat",
+        backgroundImage: `url("data:image/svg+xml;utf8,%3Csvg viewBox=%220 0 1000 1000%22 xmlns=%22http:%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3Cdefs%3E%3CclipPath id=%22a%22%3E%3Cpath fill=%22currentColor%22 d=%22M763.5 651.5Q742 803 583 818.5T341.5 725Q259 616 183 463.5t84.5-217Q428 182 546 238t178.5 159q60.5 103 39 254.5Z%22%2F%3E%3C%2FclipPath%3E%3C%2Fdefs%3E%3Cg clip-path=%22url(%23a)%22%3E%3Cpath fill=%22none%22 stroke=%22%23ffb84d%22 stroke-width=%2220%22 d=%22M763.5 651.5Q742 803 583 818.5T341.5 725Q259 616 183 463.5t84.5-217Q428 182 546 238t178.5 159q60.5 103 39 254.5Z%22%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E")`,
+      }}
+      className={className}
+    ></div>
+  );
+}
+
+export function BlurGradientBackground({ className }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.1"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      width="100%"
+      height="100%"
+      className={className}
+    >
+      <defs>
+        <linearGradient
+          gradientTransform="rotate(294, 0.5, 0.5)"
+          x1="50%"
+          y1="0%"
+          x2="50%"
+          y2="100%"
+          id="ffflux-gradient"
+        >
+          <stop stopColor="#ffff4d" stopOpacity="1" offset="0%"></stop>
+          <stop stopColor="#ffffff" stopOpacity="1" offset="100%"></stop>
+        </linearGradient>
+        <filter
+          id="ffflux-filter"
+          x="-20%"
+          y="-20%"
+          width="140%"
+          height="140%"
+          filterUnits="objectBoundingBox"
+          primitiveUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feTurbulence
+            type="fractalNoise"
+            baseFrequency="0.001 0.001"
+            numOctaves="1"
+            seed="221"
+            stitchTiles="stitch"
+            x="0%"
+            y="0%"
+            width="100%"
+            height="100%"
+            result="turbulence"
+          ></feTurbulence>
+          <feGaussianBlur
+            stdDeviation="0 0"
+            x="0%"
+            y="0%"
+            width="100%"
+            height="100%"
+            in="turbulence"
+            edgeMode="duplicate"
+            result="blur"
+          ></feGaussianBlur>
+          <feBlend
+            mode="color-dodge"
+            x="0%"
+            y="0%"
+            width="100%"
+            height="100%"
+            in="SourceGraphic"
+            in2="blur"
+            result="blend"
+          ></feBlend>
+        </filter>
+      </defs>
+      <rect
+        width="100%"
+        height="100%"
+        fill="url(#ffflux-gradient)"
+        filter="url(#ffflux-filter)"
+      ></rect>
+    </svg>
+  );
+}
