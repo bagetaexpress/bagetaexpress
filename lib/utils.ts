394387da9bf2060315013bff633aefba9aca17c2
Tomas Zifcak <tomas.zifcak197@gmail.com>
2024-09-16 18:10:17 +0200

    test: test time in deploy 2

--- lib/utils.ts
+++ lib/utils.ts
@@ -18,6 +18,7 @@ export function getNewDate(): Date {
 
 export function getDate(date_str: string): Date {
   const date = new Date(date_str);
+  return date;
   const offset = 120 * 60 * 1000;
   const time = date.getTime() - offset;
 
