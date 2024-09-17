d30152bc263173381a000648f99bd514f0fb5f55
Tomas Zifcak <tomas.zifcak197@gmail.com>
2024-09-17 12:28:24 +0200

    feat: wrap user's nav buttons to suspend

--- app/auth/c/layout.tsx
+++ app/auth/c/layout.tsx
@@ -1,11 +1,12 @@
-import { ReactNode } from "react";
-import { ShoppingCart } from "lucide-react";
+import { ReactNode, Suspense } from "react";
+import { Loader, ShoppingCart } from "lucide-react";
 import { redirect } from "next/navigation";
 
 import { getUser } from "@/lib/user-utils";
 import { getActiveOrder } from "@/db/controllers/order-controller";
 import NavButton from "@/components/nav/nav-button";
 import NavWrapper from "@/components/nav/nav-wrapper";
+import { Button } from "@/components/ui/button";
 
 export default async function authLayout({
   children,
@@ -17,31 +18,53 @@ export default async function authLayout({
     redirect("/");
   }
 
-  const hasOrder = await getActiveOrder(user.id);
-
   return (
     <div style={{ minHeight: "100dvh" }} className="flex flex-col">
       <NavWrapper>
         <NavButton href="/auth/c/store" text="Domov" />
-        {!hasOrder && (
-          <NavButton
-            href="/auth/c/cart"
-            text="Košík"
-            Icon={ShoppingCart}
-            className="hidden sm:flex"
-          />
-        )}
-        {hasOrder && (
-          <NavButton
-            href="/auth/c/order"
-            text="Objednávka"
-            className="hidden sm:flex"
-          />
-        )}
+        <Suspense
+          fallback={
+            <Button variant="ghost" className="opacity-50" disabled>
+              <Loader className="w-5 h-5 animate-spin" />
+            </Button>
+          }
+        >
+          <NavButtons />
+        </Suspense>
       </NavWrapper>
       <div className="p-2 flex-1 flex">
         <main className="max-w-screen-lg mx-auto flex-1">{children}</main>
       </div>
     </div>
+  );
+}
+
+async function NavButtons() {
+  const user = await getUser();
+  if (!user || !user.isCustomer) {
+    return null;
+  }
+
+  const hasOrder = await getActiveOrder(user.id);
+
+  return (
+    <>
+      <NavButton href="/auth/c/store" text="Domov" />
+      {!hasOrder && (
+        <NavButton
+          href="/auth/c/cart"
+          text="Košík"
+          Icon={ShoppingCart}
+          className="hidden sm:flex"
+        />
+      )}
+      {hasOrder && (
+        <NavButton
+          href="/auth/c/order"
+          text="Objednávka"
+          className="hidden sm:flex"
+        />
+      )}
+    </>
   );
 }
