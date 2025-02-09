995b90da7e59505bce2bc2d030eb0e250d469b32
Tomas Zifcak <tomas.zifcak197@gmail.com>
2025-02-09 18:30:49 +0100

    fix: auth/s/summary filter not working 2

--- app/auth/s/summary/server-util.ts
+++ app/auth/s/summary/server-util.ts
@@ -5,5 +5,5 @@ import { redirect } from "next/navigation";
 
 export async function handleFilterChange(e: FormData) {
   const filter = e.get("filter") as Order["status"];
-  redirect(process.env.NEXTAUTH_URL + `/auth/s/summary?filter=${filter}`);
+  redirect(`/auth/s/summary?filter=${filter}`);
 }
